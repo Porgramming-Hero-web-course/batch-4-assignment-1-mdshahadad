3570a67b4fa1591d6fa8ae3681f3e8b5f92ccbea
mdshahadad <identity.shahadad@gmail.com>
2024-11-28 04:48:30 +0600

    add the function with passing the arr

---
+++ problem-2.ts
@@ -0,0 +1,9 @@
+const removeDuplicates = (num: number[]): number[] => {
+    console.log(num);
+    return num;
+
+}
+
+const arr: number[] = [1, 2, 2, 3, 4, 4, 5];
+
+const newArr = removeDuplicates(arr);
