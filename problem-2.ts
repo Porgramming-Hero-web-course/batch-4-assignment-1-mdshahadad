1755bdd50e73152ddd307292b32818a80a1cfbc5
mdshahadad <identity.shahadad@gmail.com>
2024-11-28 05:12:06 +0600

    Using splice to remove the duplicate number from the array

--- problem-2.ts
+++ problem-2.ts
@@ -1,9 +1,20 @@
 const removeDuplicates = (num: number[]): number[] => {
-    console.log(num);
+
+    for (let i: number = 0; i < num.length; i++) {
+        for (let j: number = i + 1; j < num.length; j++) {
+            if (num[i] === num[j]) {
+                num.splice(j, 1);
+                j--;
+                // console.log("inside: ", num)
+            }
+        }
+    }
+    // console.log("NUm:", num)
     return num;
 
 }
 
-const arr: number[] = [1, 2, 2, 3, 4, 4, 5];
+const arr: number[] = [1, 2, 2, 3, 4, 4, 5, 6, 5, 5, 2];
 
 const newArr = removeDuplicates(arr);
+// console.log(newArr);
