953483e83faa6568775cbbb088b528c3d2293b3d
mdshahadad <identity.shahadad@gmail.com>
2024-11-29 01:18:41 +0600

    Update the problem number 6

--- problem-6.ts
+++ problem-6.ts
@@ -6,10 +6,18 @@ interface Profile {
 
 const updateProfile = (profile: Profile, partial: any): Profile => {
     console.log(profile, partial);
-    profile.age = partial.age;
+    if(partial.name !== undefined){
+        profile.name = partial.name;
+    }
+    if(partial.age !== undefined){
+        profile.age = partial.age;
+    }
+    if(partial.email !== undefined){
+        profile.email = partial.email;
+    }
     return profile;
 }
 
 
 const myProfile: Profile = { name: "Alice", age: 25, email: "alice@example.com" };
-console.log(updateProfile(myProfile, { age: 28 }));
+console.log(updateProfile(myProfile, { age: 28, name: 'Rohid' }));
