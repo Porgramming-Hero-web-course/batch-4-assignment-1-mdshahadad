d0654b3d033f5498df3f4e450040bdef245bb0f6
mdshahadad <identity.shahadad@gmail.com>
2024-11-29 01:26:12 +0600

    Update the logic when there is not matching key the give the false otherwise give the true bcz others are matched

--- problem-8.ts
+++ problem-8.ts
@@ -1,17 +1,19 @@
-const validateKeys = <T extends object>(obj: T, keys: (keyof T)[]) => {
-    for (let key of keys) {
-        if (key in obj) {
-            return true;
+{
+    const validateKeys = <T extends object>(obj: T, keys: (keyof T)[]): boolean => {
+        for (let key of keys) {
+            if (!(key in obj)) {
+                return false;
+            }
         }
+        return true;
     }
-    // console.log(obj, keys);
-}
 
-interface Person {
-    name: string;
-    age: number;
-    email: string;
-}
+    interface Person {
+        name: string;
+        age: number;
+        email: string;
+    }
 
-const person: Person = { name: "Alice", age: 25, email: "alice@example.com" };
-console.log(validateKeys(person, ["name", 'email']));
+    const person: Person = { name: "Alice", age: 25, email: "alice@example.com" };
+    console.log(validateKeys(person, ["name", 'email']));
+}
